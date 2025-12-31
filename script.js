bbb3f0c58000ead019148c8028e48d4fabae267f
UtsahEducation <satya27june@gmail.com>
2025-12-31 12:11:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Update year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Mobile menu toggle
+const toggle = document.getElementById("menuToggle");
+const nav = document.getElementById("navLinks");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+
